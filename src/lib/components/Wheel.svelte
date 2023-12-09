ab172891cda054b6e77b802127b8d7cd807ba831
Gunnar Omander <gunnar.omander@gmail.com>
2023-12-09 17:46:31 +0100

    Create a wheel component that draws a rectangle on the page

---
+++ src/lib/components/Wheel.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { onMount } from 'svelte'
+
+  let canvas: HTMLCanvasElement
+
+  onMount(() => {
+    const context = canvas.getContext('2d')!
+    context.fillStyle = 'black'
+    context.fillRect(0, 0, canvas.width, canvas.height)
+  })
+</script>
+
+<canvas
+  width="700"
+  height="700"
+  bind:this={canvas}
+/>

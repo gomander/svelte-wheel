4c9895e4b24294f5f6837f384303df4987d0385a
Gunnar Omander <gunnar.omander@gmail.com>
2023-12-15 22:59:36 +0100

    Add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import { page } from '$app/stores'
+</script>
+
+<main class="min-h-screen flex flex-col justify-center items-center gap-4 text-3xl">
+  <h1 class="h1">{$page.status}</h1>
+  <p>{$page.error?.message || 'There was an error loading the page'}</p>
+  <a href="/" class="btn variant-filled-primary">Go home</a>
+</main>

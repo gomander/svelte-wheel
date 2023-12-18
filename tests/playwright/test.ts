9212175d94f9128c04d0101d188a589e64dc3d54
Gunnar Omander <gunnar.omander@gmail.com>
2023-12-18 15:30:37 +0100

    Add simple integration tests for the home page

--- tests/playwright/test.ts
+++ tests/playwright/test.ts
@@ -1,6 +1,35 @@
 import { expect, test } from '@playwright/test'
 
-test('index page has expected h1', async ({ page }) => {
+test('Toolbar has page title', async ({ page }) => {
 	await page.goto('/')
 	await expect(page.getByRole('heading', { name: 'SvelteWheel' })).toBeVisible()
+})
+
+test('Wheel canvas is visible', async ({ page }) => {
+  await page.goto('/')
+  await expect(page.getByLabel('Wheel', { exact: true })).toBeVisible()
+})
+
+test('Entries textbox has default entries', async ({ page }) => {
+  await page.goto('/')
+  const entriesTextbox = page.getByRole('textbox', { name: 'Entries' })
+  await expect(entriesTextbox).toBeVisible()
+  await expect(entriesTextbox).toHaveValue(
+    'Ali\nBeatriz\nCharles\nDiya\nEric\nFatima\nGabriel\nHanna'
+  )
+})
+
+test('Shuffle and Sort buttons change entry order', async ({ page }) => {
+  await page.goto('/')
+  const entriesTextbox = page.getByRole('textbox', { name: 'Entries' })
+  const shuffleButton = page.getByRole('button', { name: 'Shuffle' })
+  await shuffleButton.click()
+  await expect(entriesTextbox).not.toHaveValue(
+    'Ali\nBeatriz\nCharles\nDiya\nEric\nFatima\nGabriel\nHanna'
+  )
+  const sortButton = page.getByRole('button', { name: 'Sort' })
+  await sortButton.click()
+  await expect(entriesTextbox).toHaveValue(
+    'Ali\nBeatriz\nCharles\nDiya\nEric\nFatima\nGabriel\nHanna'
+  )
 })

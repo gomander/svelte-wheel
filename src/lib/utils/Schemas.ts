0b1d21b289b76835131b2f828cb67c62ef180416
Gunnar Omander <gunnar.omander@gmail.com>
2024-01-04 01:15:20 +0100

    Make most wheel config fields optional in wheelSchema

--- src/lib/utils/Schemas.ts
+++ src/lib/utils/Schemas.ts
@@ -1,40 +1,44 @@
 import { z } from 'zod'
-import { wheelTypes, hubSizeKeys } from '$lib/utils/WheelConfig'
+import { getNewEntryId } from '$lib/utils/Wheel'
+import { wheelTypes, hubSizeKeys, defaultColors } from '$lib/utils/WheelConfig'
 import { confettiTypes } from '$lib/utils/ConfettiLauncher'
+import { wheelVisibilities } from '$lib/utils/Api'
 
 export const wheelSchema = z.object({
   wheel: z.object({
     config: z.object({
-      type: z.enum(wheelTypes),
-      title: z.string().min(1).max(50),
-      description: z.string().max(200),
-      spinTime: z.number().min(1).max(60),
-      indefiniteSpin: z.boolean(),
-      colors: z.array(z.string().length(7)),
-      confetti: z.enum(confettiTypes),
-      displayWinnerDialog: z.boolean(),
-      winnerMessage: z.string().max(50),
-      hubSize: z.enum(hubSizeKeys),
-      duringSpinSound: z.string().max(100),
-      duringSpinSoundVolume: z.number().min(0).max(1),
-      afterSpinSound: z.string().max(100),
-      afterSpinSoundVolume: z.number().min(0).max(1),
-      image: z.string()
+      type: z.optional(z.enum(wheelTypes)).default('color'),
+      title: z.string().min(1).max(50).trim(),
+      description: z.optional(z.string().max(200).trim()).default(''),
+      spinTime: z.optional(z.number().min(1).max(60)).default(10),
+      indefiniteSpin: z.optional(z.boolean()).default(false),
+      colors: z.optional(
+        z.array(z.string().trim().length(7).startsWith('#'))
+      ).default(defaultColors),
+      confetti: z.optional(z.enum(confettiTypes)).default('off'),
+      displayWinnerDialog: z.optional(z.boolean()).default(true),
+      winnerMessage: z.optional(z.string().max(50).trim()).default(''),
+      hubSize: z.optional(z.enum(hubSizeKeys)).default('S'),
+      duringSpinSound: z.optional(z.string().max(100).trim()).default(''),
+      duringSpinSoundVolume: z.optional(z.number().min(0).max(1)).default(0),
+      afterSpinSound: z.optional(z.string().max(100).trim()).default(''),
+      afterSpinSoundVolume: z.optional(z.number().min(0).max(1)).default(0),
+      image: z.optional(z.string().trim()).default('')
     }),
     entries: z.array(
       z.object({
         text: z.string(),
-        id: z.string().min(5).max(20)
+        id: z.optional(z.string().trim().length(8)).default(getNewEntryId())
       })
     )
   }),
-  visibility: z.enum(['public', 'private']),
-  uid: z.string()
+  visibility: z.enum(wheelVisibilities),
+  uid: z.string().trim().min(20).max(40)
 })
 
 export const emailValidator = z.string().email()
 
 export const userSchema = z.object({
   email: emailValidator,
-  password: z.string().min(8, 'Password must be at least 8 characters').trim()
+  password: z.string().min(8).trim()
 })

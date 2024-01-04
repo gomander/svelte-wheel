ffafdd726dd7c026a4f47a9163a86bb1e4fdd701
Gunnar Omander <gunnar.omander@gmail.com>
2024-01-04 19:33:54 +0100

    Move client-only dependencies to devDependencies

--- src/lib/server/Thumbnail.ts
+++ src/lib/server/Thumbnail.ts
@@ -3,9 +3,9 @@ import WheelPainter from '$lib/server/WheelPainter'
 import type Wheel from '$lib/utils/Wheel'
 
 export const createThumbnail = async (wheel: Wheel) => {
-  // const wheelPainter = new WheelPainter()
+  const wheelPainter = new WheelPainter()
   const canvas = createCanvas(300, 300)
-  // const context = canvas.getContext('2d')
-  // await wheelPainter.draw(context, wheel)
+  const context = canvas.getContext('2d')
+  await wheelPainter.draw(context, wheel)
   return canvas.toBuffer('image/png')
 }

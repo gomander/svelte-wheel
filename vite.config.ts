4226a5ecfe22d2130fdd6fc73052b410fe6c3c36
Gunnar Omander <gunnar.omander@gmail.com>
2024-01-18 20:58:26 +0100

    Add minimum required assets for installable PWA

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,44 @@ export default defineConfig({
 	plugins: [
     sveltekit(),
     purgeCss(),
-    SvelteKitPWA()
+    SvelteKitPWA({
+      includeAssets: [
+        'favicon.ico',
+        'apple-touch-icon.png',
+        'mask-icon.svg',
+        'robots.txt'
+      ],
+      manifest: {
+        name: 'Svelte Wheel',
+        short_name: 'SvelteWheel',
+        description: 'Enter texts onto the wheel and spin for a random result!',
+        theme_color: '#022a4f',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      }
+    })
   ],
   test: { dir: 'tests/unit' }
 })
